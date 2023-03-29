d55088bd59da08070ae9a23ef4c7f4a5bab36b0e
Bagery001 li <1197568670@qq.com>
2023-03-29 03:49:37 +0000

    M

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from datetime import datetime
+import datetime
 
 app = Flask(__name__)
 
@@ -7,19 +7,14 @@ messages = []
 
 @app.route('/')
 def index():
-    return render_template('index.html.jinja2')
+    return render_template('index.html.jinja2', messages=messages)
 
 @app.route('/post/add/', methods=['POST'])
 def add_post():
-    content = request.form['content']
-    if content:
-        messages.append((datetime.now(), content))
+    message = request.form['message']
+    timestamp = datetime.datetime.now()
+    messages.append((message, timestamp))
     return redirect(url_for('index'))
-
-@app.context_processor
-def inject_messages():
-    sorted_messages = sorted(messages, key=lambda x: x[0], reverse=True)
-    return dict(messages=sorted_messages)
 
 if __name__ == '__main__':
     app.run(debug=True)
